2bc766bb9e50086b01ebc39ec9c8f9916896d154
flnzba <florian.zeba@proton.me>
2024-10-24 13:54:15 +0200

    ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import theRemarkPlugin from "src/remark/the-remark-plugin";
-import astroRemark from '@astrojs/markdown-remark';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({applyBaseStyles: false},), sitemap(), mdx({remarkPlugins: [theRemarkPlugin]})],
+  integrations: [tailwind({applyBaseStyles: false},), sitemap(), mdx({})],
   site: 'https://www.fzeba.com',
 });
